7552b94f30065902a255acbb998502bcc5d39c8d
Петрулин Александр <sasha@besmart.by>
2022-11-16 12:15:19 +0300

    Add deployment (Homework 2)

--- src/main/java/com/otus/testservice/controller/PaymentsController.java
+++ src/main/java/com/otus/testservice/controller/PaymentsController.java
@@ -2,6 +2,7 @@ package com.otus.testservice.controller;
 
 
 import com.otus.testservice.domain.response.HealthResponse;
+import com.otus.testservice.domain.response.StudentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,5 +16,11 @@ public class PaymentsController {
     public ResponseEntity<HealthResponse> getConsolidatePayments() {
             return ResponseEntity.ok(new HealthResponse("OK"));
     }
+
+    @GetMapping(path = "/student/{studentName}")
+    public ResponseEntity<StudentResponse> getConsolidatePayments(@PathVariable("studentName") String studentName) {
+        return ResponseEntity.ok(new StudentResponse(studentName));
+    }
+
 
 }
---
+++ src/main/java/com/otus/testservice/domain/response/StudentResponse.java
@@ -0,0 +1,10 @@
+package com.otus.testservice.domain.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StudentResponse {
+    private String name;
+}
